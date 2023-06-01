c6cd62ccfc61cad9558769460c57d952eeea4d0e
KhidagrimR <tristan.charial@gmail.com>
2023-06-01 17:02:25 +0200

    Shoot particle

--- bulletManager.lua
+++ bulletManager.lua
@@ -1,7 +1,7 @@
 BulletManager = {}
 
 function BulletManager:load()
-	self.bulletLifeSpan = 2
+    self.bulletLifeSpan = 2
     self.bulletSpeed = 200
     self.bullets = {}
     self.bulletIndex = 1
@@ -11,6 +11,15 @@ function BulletManager:load()
     self.bulletSpawnOffset.x = 15
     self.bulletSpawnOffset.y = 0
 
+    --this is us setting our new particle system
+    local img = love.graphics.newImage("assets/player/particle/shoot_4.png")
+    self.pSystem = love.graphics.newParticleSystem(img, 32)
+    self.pSystem:setParticleLifetime(1, 5)
+    self.pSystem:setLinearAcceleration(-20, -20, 20, 20)
+    self.pSystem:setSpeed(20)
+    self.pSystem:setRotation(10, 20)
+    self.pSystem:setSpin(20, 50)
+
     print("Bullet Manager Loaded")
 end
 
@@ -18,26 +27,28 @@ function BulletManager:spawnBullet(direction)
     local dir = 1
     local angle
 
-    if direction == "left" then 
+    if direction == "left" then
         dir = -1
-        angle =  math.random(-self.bulletMaxSpawnAngleDegree, self.bulletMaxSpawnAngleDegree)
-    else 
+        angle = math.random(-self.bulletMaxSpawnAngleDegree, self.bulletMaxSpawnAngleDegree)
+    else
         dir = 1
         angle = math.random(-self.bulletMaxSpawnAngleDegree, self.bulletMaxSpawnAngleDegree)
     end
 
     angle = angle * (math.pi / 180) -- to convert in radiant
 
-    print("Attempt to create a bullet : "..self.bulletIndex.." on coordinate ("..Player.x.." , "..Player.y..") with direction toward : "..dir)
+    print("Attempt to create a bullet : " ..
+    self.bulletIndex .. " on coordinate (" .. Player.x .. " , " .. Player.y .. ") with direction toward : " .. dir)
 
     local bulletArrayIndex = BulletManager:getNextBulletArrayIndex()
-    self.bullets[bulletArrayIndex] = Bullet.new(self.bulletIndex, Player.x + (self.bulletSpawnOffset.x * dir), Player.y + self.bulletSpawnOffset.y, dir, angle)
-    
-    self.bulletIndex = self.bulletIndex + 1
-end
+    self.bullets[bulletArrayIndex] = Bullet.new(self.bulletIndex, Player.x + (self.bulletSpawnOffset.x * dir),
+        Player.y + self.bulletSpawnOffset.y, dir, angle)
 
-function BulletManager:test()
-    print("hello on "..self.bulletIndex)
+    self.bulletIndex = self.bulletIndex + 1
+
+
+    self.pSystem:setPosition(Player.x + (self.bulletSpawnOffset.x * dir), Player.y + self.bulletSpawnOffset.y)
+    self.pSystem:emit(32)
 end
 
 function BulletManager:getNextBulletArrayIndex()
@@ -49,13 +60,12 @@ function BulletManager:getNextBulletArrayIndex()
     return index
 end
 
-
 function BulletManager:update(dt)
-	BulletManager:updateBullets(dt)
+    self:updateBullets(dt)
+    self.pSystem:update(dt)
 end
 
 function BulletManager:updateBullets(dt)
-
     for i = 1, #self.bullets, 1 do -- #self.bullets = table.length
         if self.bullets[i] ~= nil then
             self.bullets[i]:update(dt)
@@ -63,9 +73,8 @@ function BulletManager:updateBullets(dt)
     end
 end
 
-
 function BulletManager:removeBullet(p_bulletNameIndex)
-    print("bullet removed : "..p_bulletNameIndex)
+    print("bullet removed : " .. p_bulletNameIndex)
     local bulletToRemoveIndex = nil
     for i = 1, #self.bullets, 1 do
         if self.bullets[i] ~= nil then
@@ -80,12 +89,14 @@ function BulletManager:removeBullet(p_bulletNameIndex)
         --self.bullets[bulletToRemoveIndex] = nil
         table.remove(self.bullets, bulletToRemoveIndex)
     end
-    
-    print("amount of bullet in table : "..#self.bullets)
+
+    print("amount of bullet in table : " .. #self.bullets)
 end
 
-
 function BulletManager:draw()
+    local x, y = self.pSystem:getPosition()
+    love.graphics.draw(self.pSystem, Player.x / 4, Player.y / 4)
+
     -- for each bullet, call bullet draw
     for i = 1, #self.bullets, 1 do -- #self.bullets = table.length
         if self.bullets[i] ~= nil then
@@ -93,5 +104,3 @@ function BulletManager:draw()
         end
     end
 end
-
-
